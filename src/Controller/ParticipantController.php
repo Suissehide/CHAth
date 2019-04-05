1764edd56c967c1b56c8467b6f94c6500342f994
Léo Couffinhal <leo.couffinhal1@gmail.com>
2019-04-05 23:28:33 +0200

    index

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+
+use App\Form\ParticipantType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participant/{id}", name="participant")
+     */
+    public function index(Participant $participant, Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('validation')->isClicked()) {
+                $participant = $form->getData();
+                $em->flush();
+            }
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+            'participant' => $participant,
+            'form' => $form->cretaeView(),
+        ]);
+    }
+}
