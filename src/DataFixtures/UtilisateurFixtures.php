edd728ea2b3d1a9d9095aeaf1ac13033fba0a9be
Léo Couffinhal <leo.couffinhal1@gmail.com>
2019-05-22 14:32:13 +0200

    user

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -18,10 +18,24 @@ class UtilisateurFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $roles[] = 'ROLE_USER';
 
+        $roles[] = 'ROLE_ADMIN';
         $utilisateur = new Utilisateur();
         $utilisateur->setEmail("admin@qwetle.fr");
+        $utilisateur->setNom("Couffinhal");
+        $utilisateur->setPrenom("Léo");
+        $utilisateur->setPassword($this->passwordEncoder->encodePassword(
+            $utilisateur,
+            'Admin123'
+        ));
+        $utilisateur->setRoles($roles);
+        $manager->persist($utilisateur);
+
+        $roles[] = 'ROLE_SUPER_ADMIN';
+        $utilisateur = new Utilisateur();
+        $utilisateur->setEmail("thierry.couffinhal@free.fr");
+        $utilisateur->setNom("Couffinhal");
+        $utilisateur->setPrenom("Thierry");
         $utilisateur->setPassword($this->passwordEncoder->encodePassword(
             $utilisateur,
             'Admin123'
