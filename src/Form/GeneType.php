ce476d6a1949334754c35876b1d2fcafbe716f9b
Léo Couffinhal <leo.couffinhal1@gmail.com>
2019-09-18 03:30:29 +0200

    error maj

--- src/Form/GeneType.php
+++ src/Form/GeneType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class GeneType extends AbstractType
